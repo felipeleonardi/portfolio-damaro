de243d9185fd7b0c7fc7208cfbc9ae1c821ed32b
Felipe Damaro <felipedamaro@hotmail.com>
2021-11-17 20:45:00 -0300

    added cover and header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderModule } from './components/header/header.module';
+import { CoverModule } from './components/cover/cover.module';
+
+const components = [
+  HeaderModule,
+  CoverModule
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    ...components
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/cover/cover.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CoverComponent } from './cover.component';
+
+
+
+@NgModule({
+  declarations: [
+    CoverComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CoverComponent
+  ]
+})
+export class CoverModule { }
