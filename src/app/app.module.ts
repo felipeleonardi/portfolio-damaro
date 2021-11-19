6465e556fb121b1d92c26c835373b2955b5ae978
Felipe Damaro <felipedamaro@hotmail.com>
2021-11-19 14:42:27 -0300

    added skills and education

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { HeaderModule } from './components/header/header.module';
 import { CoverModule } from './components/cover/cover.module';
 import { AboutModule } from './sections/about/about.module';
 import { HistoricModule } from './sections/historic/historic.module';
+import { SkillsModule } from './sections/skills/skills.module';
+import { EducationModule } from './sections/education/education.module';
 
 const components = [
   HeaderModule,
   CoverModule,
   AboutModule,
-  HistoricModule
+  HistoricModule,
+  SkillsModule,
+  EducationModule
 ]
 
 @NgModule({
